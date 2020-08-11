dd813c083d7f97f86b93159653908322636e1ee8
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-08-11 15:43:13 -0700

    refactoring test

--- javascript/code-challenges/arrayShift/arrayShift.test.js
+++ javascript/code-challenges/arrayShift/arrayShift.test.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const insertShiftArray = (arr, value) => {
-  console.log(arr, value);
-  // return arr;
+  let length = Math.ceil(arr.length / 2);
+
+  arr.splice(length, 0, value);
 };
 
 
@@ -12,13 +13,37 @@ const insertShiftArray = (arr, value) => {
 /* TEST for arrayShift.test.js below
 ------------------------------------------------------------------------------------------------ */
 
-// describe('Challenge-02', () => {
-it('Place outside value in middle of the array', () => {
+it('Length of array should be original array plus one', () => {
   const a = [1, 2, 3, 4];
   const b = 0;
+
+  expect(a).toStrictEqual(a);
   insertShiftArray(a, b);
-  expect(a).toStrictEqual([1, 2, 0, 3, 4]);
+
   expect(a).toHaveLength(5);
   expect(a[2]).toStrictEqual(0);
 });
-// })
+
+it('Middle index number should be ', () => {
+  const a = [9, 8, 10, 12, 0, -2, 99];
+  const b = 0;
+
+  expect(a).toStrictEqual(a);
+  insertShiftArray(a, b);
+
+  expect(a).toHaveLength(8);
+  expect(a[4]).toStrictEqual(0);
+});
+
+it('Original array is untouched/ not mutated ', () => {
+  const a = [9, 8, 10, 12, 0, -2, 99];
+  const b = [1, 2, 3, 4];
+  const c = 0;
+
+  expect(a).toStrictEqual(a);
+  insertShiftArray(a, b);
+
+  expect(a).toHaveLength(8);
+  expect(a[4]).toStrictEqual(0);
+});
+
