ae3df43e8713a9e8c61c5339c728d86559714e45
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-08-12 00:06:27 -0700

    all three tests pass and

--- javascript/code-challenges/arrayShift/arrayShift.test.js
+++ javascript/code-challenges/arrayShift/arrayShift.test.js
@@ -1,13 +1,21 @@
 'use strict';
 
 const insertShiftArray = (arr, value) => {
-  let length = Math.ceil(arr.length / 2);
+  const newArr = [];
+  const middle = Math.ceil(arr.length / 2);
 
-  arr.splice(length, 0, value);
+  for(let i = 0; i < middle; i++){
+    newArr.push(arr[i]);
+  }
+
+  newArr.push(value);
+
+  for(let i = middle; i < arr.length; i++){
+    newArr.push(arr[i]);
+  }
+
+  return newArr;
 };
-
-
-
 
 
 /* TEST for arrayShift.test.js below
@@ -18,11 +26,8 @@ it('Length of array should be original array plus one', () => {
   const b = [1, 2, 3, 4];
   const c = 0;
 
-  insertShiftArray(a, c);
-  insertShiftArray(b, c);
-
-  expect(a).toHaveLength(8);
-  expect(b).toHaveLength(5);
+  expect(insertShiftArray(a, c)).toHaveLength(8);
+  expect(insertShiftArray(b, c)).toHaveLength(5);
 });
 
 it('Middle index number replaced with Value (odd num arrays round up) ', () => {
@@ -30,11 +35,8 @@ it('Middle index number replaced with Value (odd num arrays round up) ', () => {
   const b = [1, 2, 3, 4];
   const c = 0;
 
-  insertShiftArray(a, c);
-  insertShiftArray(b, c);
-
-  expect(a[4]).toStrictEqual(0);
-  expect(b[2]).toStrictEqual(0);
+  expect(insertShiftArray(a, c)[4]).toStrictEqual(0);
+  expect(insertShiftArray(b, c)[2]).toStrictEqual(0);
 });
 
 it('Original array should be untouched/ not mutated ', () => {
@@ -45,7 +47,6 @@ it('Original array should be untouched/ not mutated ', () => {
   insertShiftArray(a, c);
   insertShiftArray(b, c);
 
-  expect(a).toStrictEqual([9, 8, 10, 12, 0, -2, 99]);
+  expect(a).toStrictEqual([9, 8, 10, 12, -1, -2, 99]);
   expect(b).toStrictEqual([1, 2, 3, 4]);
 });
-
