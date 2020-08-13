178f3c386d2e400e03115ce451457ec219973baf
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-08-13 04:01:43 -0700

    copied repl code into vs code. currently not exactly working but first step find target is working as expected.

--- javascript/code-challenges/arrayBinarySearch/array-binary-search.test.js
+++ javascript/code-challenges/arrayBinarySearch/array-binary-search.test.js
@@ -1,28 +1,55 @@
 'use strict';
 
+// let targetEx = 5;
+// let targetEx = 16;
+let targetEx = 11;
+let arrEx = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+//--Index----0--1--2--3--4--5--6---7---8---9--10--11--12--13--14--15---16
+//--Key------------*-------------------M---------------------------------
 
+function split(arr, target){
+  let initSplit = Math.ceil((arr.length / 2) - 1);
+  let useIndex = arr[initSplit];
+  console.log(useIndex);
 
-const array = (arr, value) => {
-  const findIndex = Math.ceil((arr.length / 2)-1);
-  let middle = arr[findIndex];
-  // console.log(middle);
+  for(let i = 0; i < 1; i++){
+    if(target < useIndex){
+      console.log('less');
+    }
 
-  if(value > middle){
-    console.log('more');
+    if(target > useIndex){
+      console.log('greater');
+    }
+
+    if(target === useIndex){
+      console.log('equal: FOUND!');
+      console.log(`Target: ${targetEx}, Found Number: ${useIndex}, Index Found: ${initSplit}`);
+
+      // found()
+    }
   }
 
-  if(value < middle){
-    // console.log('less');
-    // console.log(middle);
-    let newArr = findIndex / 2;
-    console.log(newArr);
-  }
-  // console.log(middle);
-};
 
-function split(start, mid){
-  let newArr = mid / 2;
-  if()
+}
+
+split(arrEx, targetEx);
+//LOOKING AT INDEX position until the end and then do value of index
+
+
+
+//Helper functions ----------------
+
+function less(){
+  let a = Math.ceil(a / 2);
+}
+
+function more(){
+  let b = arr.length
+  let a = Math.ceil(a / 2);
+}
+
+function found(){
+  return initSplit;
 }
 
 // while (middle !== value) {
@@ -42,5 +69,5 @@ function split(start, mid){
 it('check', () => {
   let arr = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
   let value = 5;
-  expect(array(arr, value)).toStrictEqual(2);
+  expect(split(arr, value)).toStrictEqual(2);
 });
