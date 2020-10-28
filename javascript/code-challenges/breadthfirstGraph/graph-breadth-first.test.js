fd1ac96be21801ce57ee2b3c8aeb2263c7e990c4
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-10-27 23:32:30 -0700

    array BS code working and tests passing

--- javascript/code-challenges/breadthfirstGraph/graph-breadth-first.test.js
+++ javascript/code-challenges/breadthfirstGraph/graph-breadth-first.test.js
@@ -1,14 +1,26 @@
 'use strict';
 
-class Edge {
 
-    constructor(vertex, weight) {
-      this.vertex = vertex;
-      this.weight = weight;
+function depthFirst(node, visited) {
+  visited.add(node);
+
+  const neighbors = this.getNeighbors(node);
+  for (let edge of neighbors) {
+    if (!visited.has(edge.vertex)) {
+      depthFirst(edge.vertex, visited);
     }
+  }
 }
 
-class Vertex { //Node
+class Edge {
+  constructor(vertex, weight) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Vertex {
+  //Node
 
   constructor(value) {
     this.value = value;
@@ -16,7 +28,6 @@ class Vertex { //Node
 }
 
 class Graph {
-
   constructor() {
     this.connections = new Map();
   }
@@ -26,8 +37,11 @@ class Graph {
   }
 
   addDirectedEdge(startVertex, endVertex, weight = 0) {
-    if (!this.connections.has(startVertex) || !this.connections.has(endVertex)) {
-      throw new Error('__ERROR__ Invalid Nodes');
+    if (
+      !this.connections.has(startVertex) ||
+      !this.connections.has(endVertex)
+    ) {
+      throw new Error('Error: Nodes');
     }
 
     const adjacencies = this.connections.get(startVertex);
@@ -36,36 +50,33 @@ class Graph {
 
   getNeighbors(vertex) {
     if (!this.connections.has(vertex)) {
-      throw new Error('__ERROR__ Invalid Vertex', vertex);
+      throw new Error('Error: Vertex', vertex);
     }
 
     return [...this.connections.get(vertex)];
   }
 
   bfs(startNode) {
-
     const queue = [];
-    const visitedNodes = new Set(); // why array NOT ideal?
+    const visitedNodes = new Set();
 
     queue.push(startNode);
-    visitedNodes.add(startNode);  
+    visitedNodes.add(startNode);
     // Big O time add to array? O(1)
     // Big O time add to set? O(1)
 
-
     while (queue.length) {
-
       const currentNode = queue.shift();
 
       const neighbors = this.getNeighbors(currentNode);
 
       for (let neighbor of neighbors) {
-
         const neighborNode = neighbor.vertex;
 
         // Big O time of checking an array for existing item? O(n)
         // Big O time of checking Set/Object (aka HashTable) ? O(1)
-        if (visitedNodes.has(neighborNode)) { // been visited????
+        if (visitedNodes.has(neighborNode)) {
+          // been visited????
           continue;
         } else {
           visitedNodes.add(neighborNode); // mark as visited now
@@ -77,6 +88,41 @@ class Graph {
     return visitedNodes;
   }
 
+  dfs(startNode) {
+    const visited = new Set();
+
+    depthFirst(startNode, visited);
+
+    return depthFirst;
+  }
+}
+
 
 /* TEST for linked-list-insert.test.js below
 ------------------------------------------------------------------------------------------------ */
+
+const graph = new Graph();
+
+const ten = new Vertex(10);
+const two = new Vertex(2);
+const six = new Vertex(6);
+const seven = new Vertex(7);
+const three = new Vertex(3);
+const eight = new Vertex(8);
+
+graph.addVertex(ten);
+graph.addVertex(two);
+graph.addVertex(six);
+graph.addVertex(seven);
+graph.addVertex(three);
+graph.addVertex(eight);
+
+graph.addDirectedEdge(ten, two);
+graph.addDirectedEdge(ten, six);
+graph.addDirectedEdge(ten, three);
+// graph.addDirectedEdge(ten, seven);
+graph.addDirectedEdge(two, seven);
+graph.addDirectedEdge(six, seven);
+graph.addDirectedEdge(six, eight);
+graph.addDirectedEdge(three, eight);
+graph.addDirectedEdge(eight, seven);
